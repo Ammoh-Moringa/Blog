c6aa28f2a303de60cd8cef611e6b5eb25758b6e7
Ammoh-Moringa <amos.kiprotich@student.moringaschool.com>
2021-06-19 17:56:09 +0300

    Add def index

---
+++ app/main/views.py
@@ -0,0 +1,99 @@
+from flask import (render_template, request, redirect, 
+                   url_for, abort)
+from . import main
+from ..models import User, Comment, Post, Subscribers
+from flask_login import login_required, current_user
+from .forms import (UpdateProfile, PostForm, 
+                    CommentForm, UpdatePostForm)
+from datetime import datetime
+# import bleach
+from .. import db
+from ..requests import get_quote
+from ..email import welcome_message, notification_message
+
+@main.route("/", methods = ["GET", "POST"])
+def index():
+    posts = Post.get_all_posts()
+    quote = get_quote()
+
+    if request.method == "POST":
+        new_sub = Subscribers(email = request.form.get("subscriber"))
+        db.session.add(new_sub)
+        db.session.commit()
+        welcome_message("Blog welcomes you to this platform", 
+                        "email/welcome", new_sub.email)
+    return render_template("index.html",
+                            posts = posts,
+                            quote = quote)
+
+@main.route("/post/<int:id>", methods = ["POST", "GET"])
+def post(id):
+    post = Post.query.filter_by(id = id).first()
+    comments = Comment.query.filter_by(post_id = id).all()
+    comment_form = CommentForm()
+    comment_count = len(comments)
+
+    if comment_form.validate_on_submit():
+        comment = comment_form.comment.data
+        comment_form.comment.data = ""
+        comment_alias = comment_form.alias.data
+        comment_form.alias.data = ""
+        if current_user.is_authenticated:
+            comment_alias = current_user.username
+        new_comment = Comment(comment = comment, 
+                            comment_at = datetime.now(),
+                            comment_by = comment_alias,
+                            post_id = id)
+        new_comment.save_comment()
+        return redirect(url_for("main.post", id = post.id))
+
+    return render_template("post.html",
+                            post = post,
+                            comments = comments,
+                            comment_form = comment_form,
+                            comment_count = comment_count)
+
+
+@main.route("/profile/<int:id>", methods = ["POST", "GET"])
+def profile(id):
+    user = User.query.filter_by(id = id).first()
+    posts = Post.query.filter_by(user_id = id).all()
+
+    if request.method == "POST":
+        new_sub = Subscribers(email = request.form.get("subscriber"))
+        db.session.add(new_sub)
+        db.session.commit()
+        welcome_message("Thank you for subscribing to the CM blog", 
+                        "email/welcome", new_sub.email)
+
+    return render_template("profile/profile.html",
+                            user = user,
+                            posts = posts)
+
+@main.route("/profile/<int:id>/<int:post_id>/delete")
+@login_required
+def delete_post(id, post_id):
+    user = User.query.filter_by(id = id).first()
+    post = Post.query.filter_by(id = post_id).first()
+    db.session.delete(post)
+    db.session.commit()
+    return redirect(url_for("main.profile", id = user.id))
+
+@main.route("/profile/<int:id>/update", methods = ["POST", "GET"])
+@login_required
+def update_profile(id):
+    user = User.query.filter_by(id = id).first()
+    form = UpdateProfile()
+    if form.validate_on_submit():
+        user.first_name = form.first_name.data
+        user.last_name = form.last_name.data
+        user.email = form.email.data
+        user.bio = form.bio.data
+
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for("main.profile", id = id))
+    
+    return render_template("profile/update.html",
+                            user = user,
+                            form = form)
--- app/models.py
+++ app/models.py
@@ -6,36 +6,32 @@ from datetime import datetime
 
 
 @login_manager.user_loader
-def load_user(user_id):
-    """
-    @login_manager.user_loader Passes in a user_id to this function
-    Function queries the database and gets a user's id as a response
-    """
+def user_loader(user_id):
     return User.query.get(int(user_id))
 
-
 class User(UserMixin, db.Model):
-    """ 
-    class modelling the users 
-    """
-
     __tablename__ = 'users'
-
-    #create the columns
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(255))
-    email = db.Column(db.String(255), unique=True, index=True)
+    id = db.Column(db.Integer, primary_key = True)
+    first_name = db.Column(db.String(255))
+    last_name = db.Column(db.String(255))
+    username = db.Column(db.String(255), unique = True)
+    email = db.Column(db.String(255), unique = True, index = True)
+    bio = db.Column(db.String())
+    avatar_path = db.Column(db.String())
     password_hash = db.Column(db.String(255))
-    bio = db.Column(db.String(255))
-    profile_pic_path = db.Column(db.String())
-    pitches = db.relationship("Pitch", backref="user", lazy="dynamic")
-    comment = db.relationship("Comments", backref="user", lazy="dynamic")
-    vote = db.relationship("Votes", backref="user", lazy="dynamic")
+    posts = db.relationship("Post",
+                            backref = "user",
+                            lazy = "dynamic")
+    comments = db.relationship("Comment",
+                                backref = "user",
+                                lazy = "dynamic")
+    liked = db.relationship("PostLike",
+                            backref = "user", 
+                            lazy = "dynamic")
 
-    # securing passwords
     @property
     def password(self):
-        raise AttributeError("You cant always get it right")
+        raise AttributeError("You cannot read the password attribute")
 
     @password.setter
     def password(self, password):
@@ -44,9 +40,28 @@ class User(UserMixin, db.Model):
     def verify_password(self, password):
         return check_password_hash(self.password_hash, password)
 
-    def __repr__(self):
-        return f'User {self.username}'
+    # User like logic
+    def like_post(self, post):
+        if not self.has_liked_post(post):
+            like = PostLike(user_id = self.id, post_id = post.id)
+            db.session.add(like)
 
+    # User dislike logic
+    def unlike_post(self, post):
+        if self.has_liked_post(post):
+            PostLike.query.filter_by(
+                user_id = self.id,
+                post_id = post.id).delete()
+
+    # Check if user has liked post
+    def has_liked_post(self, post):
+        return PostLike.query.filter(
+            PostLike.user_id == self.id,
+            PostLike.post_id == post.id).count() > 0
+
+    # string representaion to print out a row of a column, important in debugging
+    def __repr__(self):
+        return f"User {self.username}"
 
 class Post(db.Model):
     __tablename__ = "posts"
