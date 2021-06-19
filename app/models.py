ebd6882f2afc0f3aa76fca20a2cd3e6eb79ec767
Ammoh-Moringa <amos.kiprotich@student.moringaschool.com>
2021-06-19 13:59:44 +0300

    Add class post

---
+++ app/models.py
@@ -0,0 +1,83 @@
+from . import db
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from . import login_manager
+from datetime import datetime
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    """
+    @login_manager.user_loader Passes in a user_id to this function
+    Function queries the database and gets a user's id as a response
+    """
+    return User.query.get(int(user_id))
+
+
+class User(UserMixin, db.Model):
+    """ 
+    class modelling the users 
+    """
+
+    __tablename__ = 'users'
+
+    #create the columns
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(255))
+    email = db.Column(db.String(255), unique=True, index=True)
+    password_hash = db.Column(db.String(255))
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
+    pitches = db.relationship("Pitch", backref="user", lazy="dynamic")
+    comment = db.relationship("Comments", backref="user", lazy="dynamic")
+    vote = db.relationship("Votes", backref="user", lazy="dynamic")
+
+    # securing passwords
+    @property
+    def password(self):
+        raise AttributeError("You cant always get it right")
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def __repr__(self):
+        return f'User {self.username}'
+
+
+class Post(db.Model):
+    __tablename__ = "posts"
+
+    id = db.Column(db.Integer, primary_key = True)
+    post_title = db.Column(db.String)
+    post_content = db.Column(db.Text)
+    posted_at = db.Column(db.DateTime)
+    upvotes = db.Column(db.Integer, default = 0)
+    downvotes = db.Column(db.Integer, default = 0)
+    post_by = db.Column(db.String)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    comments = db.relationship("Comment", 
+                                foreign_keys = "Comment.post_id", 
+                                backref = "post", 
+                                lazy = "dynamic")
+
+    def save_post(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete_post(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    @classmethod
+    def get_user_posts(cls,id):
+        posts = Post.query.filter_by(user_id = id).order_by(Post.posted_at.desc()).all()
+        return posts
+
+    @classmethod
+    def get_all_posts(cls):
+        return Post.query.order_by(Post.posted_at).all()
+
