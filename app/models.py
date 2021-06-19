c887ceab73cde0259653973c717e71538281a5b2
Ammoh-Moringa <amos.kiprotich@student.moringaschool.com>
2021-06-19 14:19:53 +0300

    Add class for posing a like

--- app/models.py
+++ app/models.py
@@ -115,3 +115,10 @@ class Subscribers(db.Model):
     email = db.Column(db.String(255), unique = True, index = True)
 
 
+class PostLike(db.Model):
+    __tablename__ = "post_like"
+    id = db.Column(db.Integer, primary_key = True)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    post_id = db.Column(db.Integer, db.ForeignKey("posts.id"))
+
+
