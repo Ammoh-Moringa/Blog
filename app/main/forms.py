7a1dc35f143707462d8bfcf6fcee4b222962fa5a
Ammoh-Moringa <amos.kiprotich@student.moringaschool.com>
2021-06-21 20:39:22 +0300

    Add background image to quotes

--- app/main/forms.py
+++ app/main/forms.py
@@ -5,7 +5,7 @@ from wtforms.validators import Required
 
 class PostForm(FlaskForm):
     title = StringField("Blog title:", validators=[Required()])
-    post = TextAreaField("Post your blog:", validators=[Required()])
+    post = TextAreaField("Post blog:", validators=[Required()])
     submit = SubmitField("Post")
 
 class UpdatePostForm(FlaskForm):
